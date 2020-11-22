76a486572d3f3790e1169453b425d063268d0461
rtconverter <rtconverter@gmail.com>
2020-11-22 23:49:00 +0300

    task 3

---
+++ task_3/task_3.py
@@ -0,0 +1,25 @@
+from threading import Thread, Lock
+
+a = 0
+
+
+def function(arg, lock):
+    global a
+    with lock:
+        for _ in range(arg):
+            a += 1
+
+
+def main():
+    threads = []
+    lock = Lock()
+    for i in range(5):
+        thread = Thread(target=function, args=(1000000, lock))
+        thread.start()
+        threads.append(thread)
+
+    [t.join() for t in threads]
+    print("----------------------", a)  # ???
+
+
+main()
